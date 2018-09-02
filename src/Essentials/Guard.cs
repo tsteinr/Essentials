97bfe2b94547d8b1d1289c3d7d3bd750ed0fde26
Thomas Steiner <tsteinr@gmail.com>
2018-09-02 03:27:16 +0200

    Added simple Guard

---
+++ src/Essentials/Guard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Essentials
+{
+    public static class Guard
+    {
+        public static void ArgumentNotNull(Object value, String message)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(String.IsNullOrEmpty(message) ? nameof(value) : message);
+            }
+        }
+    }
+}
